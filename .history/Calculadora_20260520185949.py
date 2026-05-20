7255d22995ab1b39adacef976f7522f4c10c7345
julliassantos-dev <jullia_s_santos@estudate.sesisenai.org.br>
2026-05-20 19:33:19 -0300

    atualização

---
+++ .history/Calculadora_20260520185949.py
@@ -0,0 +1,54 @@
+# ===========
+# calculadora
+# ===========
+
+def soma(a,b):
+   return a + b
+
+def subtracao(a,b):
+   return a - b
+
+def divisao(a,b):
+   if b == 0:
+      return "Div0"   
+   else:
+      return a / b
+ 
+def multiplicacao(a,b): 
+   return a * b
+
+def exponent(a,)
+
+while True: 
+    print(" ===== Calculadora ===== ")
+    print("1 - Soma ")
+    print("2 - Subtração ")
+    print("3 - Divisão ")
+    print("4 - Multiplicação")
+    print("0 - Sair")
+    
+    opcao = int(input("Escolha a operação desejada: "))
+
+    if opcao == 0:
+       break
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    
+    if opcao == 1:
+      resultado = soma(num1,num2) 
+      
+    elif opcao == 2:
+      resultado = subtracao(num1,num2)
+    
+    elif opcao == 3:
+      resultado = divisao(num1,num2)
+    
+    elif opcao == 4:
+      resultado = multiplicacao(num1,num2)
+    
+    else :
+       print("Opção inválida!")
+       continue
+
+    print(f"Resultado = {resultado}")
---
+++ .history/Calculadora_20260520191936.py
@@ -0,0 +1,69 @@
+# ===========
+# calculadora
+# ===========
+
+def soma(a,b):
+    return a + b
+
+def subtracao(a,b):
+    return a - b
+
+def divisao(a,b):
+    if b == 0:
+        return "Div0"   
+   
+    else:
+        return a / b
+ 
+def  multiplicacao(a,b): 
+    return a * b
+
+def exponent(a,b):
+    return a**b
+
+def porcent(a,b):
+    return (a*(b/100))
+
+
+
+while True: 
+    print(" ===== Calculadora ===== ")
+    print("1 - Soma ")
+    print("2 - Subtração ")
+    print("3 - Divisão ")
+    print("4 - Multiplicação")
+    print("5 - Exponenciação")
+    print("6 - Porcentagem Total/Percentual")
+    print("0 - Sair")
+    
+    opcao = int(input("Escolha a operação desejada: "))
+
+    if opcao == 0:
+        break
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    
+    if opcao == 1:
+        resultado = soma(num1, num2) 
+      
+    elif opcao == 2:
+        resultado = subtracao(num1, num2)
+    
+    elif opcao == 3:
+        resultado = divisao(num1, num2)
+    
+    elif opcao == 4:
+        resultado = multiplicacao(num1, num2)
+
+    elif opcao == 5:
+        resultado = exponent(num1, num2)
+ 
+    elif opcao == 6:
+        resultado = porcent(num1, num2)
+    
+    else :
+        print("Opção inválida!")
+        continue
+
+    print(f"Resultado = {resultado}")
